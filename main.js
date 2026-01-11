1c09511605ca9a93c42e1e7402b4c08c1a190837
Stefan <120310018+stefanlatic@users.noreply.github.com>
2026-01-11 16:52:42 +0100

    Add files via upload
    Updated sections commit

--- main.js
+++ main.js
@@ -1,9 +1,42 @@
-let container = document.querySelector(".product-section");
+//selection
+const allProductsContainer = document.querySelector(".products-container");
+const aboutContainer = document.querySelector(".about-section"); 
 
-container.addEventListener("wheel", (e) => {
+let hasJumped = false;
+//probaj sa aboutSection
+aboutContainer.addEventListener("wheel", (e) => {
+    if (hasJumped) return;
+
+    const section = aboutContainer.getBoundingClientRect();
+
+    //now it must be in current viewport
+    const isInView = section.top <= 0 && section.bottom > aboutContainer.innerHeight / 2;
+
+    if(!hasJumped && isInView && e.deltaY > 0)
+        e.preventDefault();
+
+    hasJumped = true;
+
+    allProductsContainer.scrollIntoView({
+        behavior: "smooth"
+    })
+}, {passive : false});
+
+aboutContainer.addEventListener("scroll", () => {
+    const section = aboutContainer.getBoundingClientRect();
+
+    if(section.top >= 0) {
+        hasJumped = false;
+    }
+})
+
+// wheel on all container
+allProductsContainer.addEventListener("wheel", (e) => {
     if (e.deltaY !== 0) {
         e.preventDefault();
-        container.scrollLeft += e.deltaY;
+        allProductsContainer.scrollLeft += e.deltaY;
     }
 }, { passive: false });
-E
+
+
+// Sad mi treba da popravim skrol na dole sa about na product sekciju i onda da se skrolom na gore vrati gore 
